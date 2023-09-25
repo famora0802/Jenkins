653b07896b638ce0ce5cd411b8d0f0383fa746b6
famora0802 <101901134+famora0802@users.noreply.github.com>
2023-09-25 14:26:57 +0200

    Update TEST_Jenkins.py

--- TEST_Jenkins.py
+++ TEST_Jenkins.py
@@ -1,4 +1,4 @@
 print("")
 print("")
-print("FELICITATIONS, Vous venez d'exécuter un code python avec JENKINS :) ")
+print("FELICITATIONS, Vous venez d'exécuter un code python avec JENKINS en utilisant la fonctionnalité git:) ")
 print("")
